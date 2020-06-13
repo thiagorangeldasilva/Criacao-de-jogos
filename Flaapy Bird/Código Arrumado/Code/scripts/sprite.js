6d6052b662a7f4ed311f331d0b5cc6d99e2f875a
Thiago Rangel <thiagodasilva925@gmail.com>
2020-06-13 20:08:06 -0300

    Desenhei as imgs do player e gamer over
    Desenhei as imagens, mas ocorreu um bug no sprite.js

--- Flaapy Bird/Código Arrumado/Code/scripts/sprite.js
+++ Flaapy Bird/Código Arrumado/Code/scripts/sprite.js
@@ -1,25 +1,19 @@
-function Sprite(x, y, largura, altura) {
+function Sprite(x, y, largura, altura, img) {
 	this.x = x;
 	this.y = y;
 	this.largura = largura;
 	this.altura = altura;
+	this.img = img
 
 	this.desenha = function(xCanvas, yCanvas) {
-		contexto.drawImage(imagem, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura);
+		contexto.drawImage(this.img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura);
 	}
 }
 
-var bg = new Sprite(0, 0, 600, 600),
-spriteBoneco = new Sprite(618, 16, 87, 87),
-
-perdeu = new Sprite(603, 478, 397, 358),
-jogar = new Sprite(603, 127, 397, 347),
-novo = new Sprite(68, 721, 287, 93),
-spriteRecord = new Sprite(28, 879, 441, 95),
-spriteChao = new Sprite(0, 604, 600, 54),
-
-redObstacle = new Sprite(662, 867, 50, 120),
-pinkObstacle = new Sprite(719, 867, 50, 120),
-blueObstacle = new Sprite(779, 867, 50, 120),
-greenObstacle = new Sprite(839, 867, 50, 120),
-yellowObstacle = new Sprite(898, 867, 50, 120);
+/*let bg = new Sprite(0, 0, 600, 600, imagem)
+let	perdeu = new Sprite(603, 478, 397, 358, imagem)
+let jogar = new Sprite(603, 127, 397, 347, imagem)
+let novo = new Sprite(68, 721, 287, 93, imagem)
+let spriteRecord = new Sprite(28, 879, 441, 95, imagem)
+let spriteChao = new Sprite(0, 604, 600, 54, imagem)
+*/let playerBloco = new Sprite(0, 0, 50, 50, imgGestrela)
